55dca1acddb11bc3cae7df5b870622dfd2011fc3
Laura Viviana Álvarez Carvajal <lvalvarezc@unal.edu.co>
2017-05-13 19:41:43 -0500

    Set start and rancher-compose files for each node

---
+++ blinkbox-node2/start-node2.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+mvn -f pom.xml package
+
+./../rancher-compose --project-name blinkbox-node2 \
+    --url http://10.0.0.4:8080/v1/projects/1a5 \
+    --access-key 3D58FDED33004FAAF787 \
+    --secret-key ZtziaK44daUzmXRaHHWJoK3jS4JFqUPhJcvSMd5J \
+    -f node2.yml \
+    --verbose up \
+    -d --force-upgrade \
+    --confirm-upgrade
---
+++ blinkbox-node3/start-node3.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+mvn -f pom.xml package
+
+./../rancher-compose --project-name blinkbox-node3 \
+    --url http://10.0.0.4:8080/v1/projects/1a5 \
+    --access-key 8DAFE51DA45D7AF3ED87 \
+    --secret-key oY7m7xbi44jQkmuZAFYpPMYzUsXNbStA8VUjn7RB \
+    -f node3.yml \
+    --verbose up \
+    -d --force-upgrade \
+    --confirm-upgrade
