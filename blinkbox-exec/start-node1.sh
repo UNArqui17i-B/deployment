1ea685160038975b3bc7510561ea23f1b77d6b04
Laura Viviana Álvarez Carvajal <lvalvarezc@unal.edu.co>
2017-05-16 17:15:52 -0500

    Set labels to services

---
+++ blinkbox-exec/start-node1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+./../rancher-compose --project-name blinkbox-nodes \
+    --url http://10.0.0.4:8080/v1/projects/1a5 \
+    --access-key 220DC56668EF08268E6E \
+    --secret-key oF9wDqENapEJLAxgE4sez5xC91Z6nEvg8pDvLhFh \
+    -f nodes.yml \
+    --verbose up \
+    -d --force-upgrade \
+    --confirm-upgrade
